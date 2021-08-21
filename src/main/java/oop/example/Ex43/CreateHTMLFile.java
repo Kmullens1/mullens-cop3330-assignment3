d4dd98db1e1f4c1f7da003d66de712653279fbcb
kmullens1 <kmullens1@knights.ucf.edu>
2021-08-21 18:39:18 -0400

    Ex43 - I think htmlFile is working properly - fix /index - incomplete

--- src/main/java/oop/example/Ex43/CreateHTMLFile.java
+++ src/main/java/oop/example/Ex43/CreateHTMLFile.java
@@ -1,30 +1,25 @@
 package oop.example.Ex43;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class CreateHTMLFile {
     public void htmlFile(String siteName, String author){
         File createHTML = new File("C:\\Users\\kmull\\Desktop\\" + siteName + ".html");
-        FileWriter fileWriter = null;
-        try {
-            fileWriter = new FileWriter(siteName + ".html");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        BufferedWriter writer = new BufferedWriter(fileWriter);
-        try {
+        try
+        {
             if(createHTML.createNewFile())
+            {
                 System.out.print("Created ./" + siteName + "/index.html");
+
+                BufferedWriter bw = new BufferedWriter(new FileWriter(createHTML));
+                bw.write("<title>" + siteName + "</title><meta>" + author + "</meta>");
+                bw.newLine();
+                bw.close();
+            }
             else
                 System.out.print("This file already exists.");
 
-            writer.write("<title>" + siteName + "<title>" + "<meta>" + author + "<meta>");
-            writer.newLine();
-            writer.close();
-        } catch (IOException e) {
+        } catch(IOException e){
             System.out.print("Unknown Error Occurred");
             e.printStackTrace();
         }
