99fd6f01945f96a45c1ecb7bbc015b16ed4dffac
kmullens1 <kmullens1@knights.ucf.edu>
2021-09-06 11:10:41 -0400

    Ex 44 - Testcases complete - update pseudocode

--- src/test/java/oop/example/Ex44/Parse_DataTest.java
+++ src/test/java/oop/example/Ex44/Parse_DataTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class Parse_DataTest {
 
     @Test
-    void searchProducts() {
+    void searchProducts_Widget() {
         ArrayList<Object> expected = new ArrayList<>();
         expected.add(true);
         expected.add("Widget");
@@ -42,4 +42,125 @@ class Parse_DataTest {
         assertEquals(expected, actual);
 
     }
+
+
+
+    @Test
+    void searchProducts_Thing() {
+        ArrayList<Object> expected = new ArrayList<>();
+        expected.add(true);
+        expected.add("Thing");
+        expected.add(15.0);
+        expected.add(5);
+
+        ArrayList<Object> actual = new ArrayList<>();
+
+        try {
+            Gson gson = new Gson();
+
+            Reader read = Files.newBufferedReader(Paths.get("src/main/java/oop/example/Ex44/Product_Data.json"));
+            InventoryList inventoryList = gson.fromJson(read, InventoryList.class);
+
+            String item = "Thing";
+            Parse_Data search = new Parse_Data();
+            actual = search.searchProducts(inventoryList, item);
+
+            read.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void searchProducts_Doodad() {
+        ArrayList<Object> expected = new ArrayList<>();
+        expected.add(true);
+        expected.add("Doodad");
+        expected.add(5.0);
+        expected.add(10);
+
+        ArrayList<Object> actual = new ArrayList<>();
+
+        try {
+            Gson gson = new Gson();
+
+            Reader read = Files.newBufferedReader(Paths.get("src/main/java/oop/example/Ex44/Product_Data.json"));
+            InventoryList inventoryList = gson.fromJson(read, InventoryList.class);
+
+            String item = "Doodad";
+            Parse_Data search = new Parse_Data();
+            actual = search.searchProducts(inventoryList, item);
+
+            read.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void searchProducts_NonExistent_Item() {
+        ArrayList<Object> expected = new ArrayList<>();
+        expected.add(false);
+
+        ArrayList<Object> actual = new ArrayList<>();
+
+        try {
+            Gson gson = new Gson();
+
+            Reader read = Files.newBufferedReader(Paths.get("src/main/java/oop/example/Ex44/Product_Data.json"));
+            InventoryList inventoryList = gson.fromJson(read, InventoryList.class);
+
+            String item = "Gadget";
+            Parse_Data search = new Parse_Data();
+            actual = search.searchProducts(inventoryList, item);
+
+            read.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        assertNotEquals(expected, actual);
+
+    }
+
+
+    @Test
+    void searchProducts_Lowercase_widget() {
+        ArrayList<Object> expected = new ArrayList<>();
+        expected.add(false);
+
+        ArrayList<Object> actual = new ArrayList<>();
+
+        try {
+            Gson gson = new Gson();
+
+            Reader read = Files.newBufferedReader(Paths.get("src/main/java/oop/example/Ex44/Product_Data.json"));
+            InventoryList inventoryList = gson.fromJson(read, InventoryList.class);
+
+            String item = "widget";
+            Parse_Data search = new Parse_Data();
+            actual = search.searchProducts(inventoryList, item);
+
+            read.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        assertNotEquals(expected, actual);
+
+    }
+
+
+
+
 }
